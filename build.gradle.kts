0944d551f28c6a3449f5da795743683deba4717e
asmasoomro <61662544+asmasoomro@users.noreply.github.com>
2023-11-16 21:42:26 +0000

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,42 @@
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-        classpath("com.ibm.watson:ibm-watson:11.0.1")
-    }
-}
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
+android {
+    namespace = "com.example.project"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.project"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation(files("libs\\java-sdk-3.3.1-jar-with-dependencies.jar"))
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
